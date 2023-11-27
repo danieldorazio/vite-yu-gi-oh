813bce479f0e33345bedb80f1ca4b99521a6573e
danieldorazio <danieldorazio.97@gmail.com>
2023-11-27 18:07:34 +0100

    add: imges in AppMain

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <script>
+import axios from "axios";
 import Header from './components/AppHeader.vue';
 import Main from './components/AppMain.vue';
+import { store } from "./store.js";
+
 
 export default {
+  data() {
+    return {
+      store
+    };
+  },
+
+  created () {
+    axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0")
+    .then((resp) => {
+      this.store.cards = resp.data.data;
+      console.log(this.store);
+    });
+  },
+
   components: {
     Header,
     Main,
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,23 +1,50 @@
 <script>
 import Selector from './AppSelector.vue';
+import { store } from "../store";
 
 export default {
     components: {
         Selector,
-    }
+    },
+
+    data () {
+        return {
+            store,
+        };
+    },
+
+    methods: {
+        getImagePath: function(img) {
+            return new URL(`${img}`, import.meta.url).href;
+        }
+    },
 }
 
 </script>
 
 <template>
     <section class="main">
-        <Selector/>
+        <Selector />
+        <div class="container">
+            <div class="row bg-white">
+                <div class="my_col col-sm-12 col-md-3" v-for="(card, index) in store.cards" >
+                    <div class="card text-center">
+                        <!-- {{ card.card_images[0].image_url}} -->
+                        <img :src="getImagePath(card.card_images[0].image_url)" alt="">
+                    </div>
+                    <div>
+                        {{ card.name }}
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
 <style scoped lang="scss">
-    @use '../style/partials/variables' as*;
-    .main {
-        background-color: $my_color_primary;
-    }
+@use '../style/partials/variables' as*;
+
+.main {
+    background-color: $my_color_primary;
+}
 </style>
